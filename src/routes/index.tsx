345e7cfc6ad3a97f6659fb9e31d2ec3ed0e16e9f
Ok-Cheese <starberry970310@gmail.com>
2022-06-21 18:35:31 +0900

    feat: create routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,15 @@
-const Routes = () => {
-  return <div />;
+import { useState } from 'react';
+
+import Loading from '../components/Loading';
+
+import styles from './routes.module.scss';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const content = isLoading ? <Loading /> : '';
+
+  return <div className={styles.app}>{content}</div>;
 };
 
-export default Routes;
+export default App;
