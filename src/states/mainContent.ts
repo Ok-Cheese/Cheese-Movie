8e18300806cccddf419dc9c1a801a577cb98d002
Ok-Cheese <starberry970310@gmail.com>
2022-06-23 20:21:40 +0900

    feat: change state name to 'content'

--- src/states/mainContent.ts
+++ src/states/mainContent.ts
@@ -3,15 +3,21 @@ import store from 'store';
 
 import { IContent } from 'types/type';
 
-const savedMoviePopular = store.get('MOVIE_RAKNING');
-const savedMovieRated = store.get('MOVIE_RATED');
+let savedContent = {
+  movie_popular: [],
+  movie_top_rated: [],
+  tv_popular: [],
+  tv_rated: [],
+};
+
+savedContent = { ...savedContent, ...store.get('MAIN_CONTENT_MOIVE') };
 
 export const moviePopularAtom = atom<IContent[]>({
   key: '#moviePopularAtom',
-  default: savedMoviePopular || null,
+  default: savedContent.movie_popular,
 });
 
 export const movieRatedAtom = atom<IContent[]>({
   key: '#movieRatedAtom',
-  default: savedMovieRated || null,
+  default: savedContent.movie_top_rated,
 });
