93fffcf079267da391f7f5a1751bf04d14d03a39
Ok-Cheese <starberry970310@gmail.com>
2022-06-25 03:37:11 +0900

    feat: add close and favorite icon

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,5 @@
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as FilterIcon } from './filter.svg';
 export { ReactComponent as StarIcon } from './star.svg';
+export { ReactComponent as CloseIcon } from './close.svg';
+export { ReactComponent as FavoriteIcon } from './heart.svg';
