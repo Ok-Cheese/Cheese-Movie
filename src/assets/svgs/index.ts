d82ae3369eac2bd963a7d4ae829b84e85eba47a4
Ok-Cheese <starberry970310@gmail.com>
2022-06-25 11:07:41 +0900

    feat: add tv and movie icon

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,3 +3,5 @@ export { ReactComponent as FilterIcon } from './filter.svg';
 export { ReactComponent as StarIcon } from './star.svg';
 export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as FavoriteIcon } from './heart.svg';
+export { ReactComponent as TvIcon } from './tv.svg';
+export { ReactComponent as MovieIcon } from './movie.svg';
