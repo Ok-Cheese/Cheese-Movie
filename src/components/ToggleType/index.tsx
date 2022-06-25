36782878f6987713b5c72459df76bc08d8e8e63d
Ok-Cheese <starberry970310@gmail.com>
2022-06-25 12:53:18 +0900

    feat: add ToggleType

---
+++ src/components/ToggleType/index.tsx
@@ -0,0 +1,69 @@
+import cx from 'classnames';
+
+import { useAppDispatch, useAppSelector } from 'hooks';
+import { TPage } from 'types/type';
+import {
+  getHomeType,
+  getSearchType,
+  setHomeTypeToMovie,
+  setHomeTypeToTv,
+  setSearchTypeToMovie,
+  setSearchTypeToTv,
+} from 'states/contentTypes';
+import { setSearchResult } from 'states/search';
+import { MovieIcon, TvIcon } from 'assets/svgs';
+
+import styles from './toggleType.module.scss';
+
+interface IProps {
+  page: TPage;
+}
+
+const ToggleType = ({ page }: IProps) => {
+  const contentType = useAppSelector(page === 'home' ? getHomeType : getSearchType);
+
+  const dispatch = useAppDispatch();
+
+  const setTypeToMovie = () => {
+    if (page === 'home') {
+      dispatch(setHomeTypeToMovie());
+      return;
+    }
+
+    dispatch(setSearchResult([]));
+    dispatch(setSearchTypeToMovie());
+  };
+
+  const setTypeToTv = () => {
+    if (page === 'home') {
+      dispatch(setHomeTypeToTv());
+      return;
+    }
+
+    dispatch(setSearchResult([]));
+    dispatch(setSearchTypeToTv());
+  };
+
+  return (
+    <div className={styles.buttonWrapper}>
+      <button
+        className={cx(styles.toggleTypeButton, { [styles.active]: contentType === 'movie' })}
+        type='button'
+        onClick={setTypeToMovie}
+      >
+        <MovieIcon />
+        영화
+      </button>
+      <button
+        className={cx(styles.toggleTypeButton, { [styles.active]: contentType === 'tv' })}
+        type='button'
+        onClick={setTypeToTv}
+      >
+        <TvIcon />
+        TV
+      </button>
+    </div>
+  );
+};
+
+export default ToggleType;
